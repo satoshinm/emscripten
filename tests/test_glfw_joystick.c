a33bc828f54e6c8bff5f677879a57cc859dd9e4e
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-04-29 12:10:19 -0700

    Begin converting to a non-interactive browser test

--- tests/test_glfw_joystick.c
+++ tests/test_glfw_joystick.c
@@ -65,6 +65,84 @@ void joystick_callback(int joy, int event)
   }
 }
 
+void main_2(void *arg) {
+  emscripten_run_script("window.addNewGamepad('Pad Thai', 4, 16)");
+  emscripten_run_script("window.addNewGamepad('Pad Kee Mao', 0, 4)");
+  // Check that the joysticks exist properly.
+  printf("glfwJoystickPresent(GLFW_JOYSTICK_1) = %d\n", glfwJoystickPresent(GLFW_JOYSTICK_1));
+  printf("glfwJoystickPresent(GLFW_JOYSTICK_2) = %d\n", glfwJoystickPresent(GLFW_JOYSTICK_2));
+  EM_ASM(alert(1));
+
+  assert(glfwJoystickPresent(GLFW_JOYSTICK_1));
+  assert(glfwJoystickPresent(GLFW_JOYSTICK_2));
+
+  // TODO
+  /*
+  assert(!SDL_JoystickOpened(0));
+  assert(!SDL_JoystickOpened(1));
+  SDL_Joystick* pad1 = SDL_JoystickOpen(0);
+  assert(SDL_JoystickOpened(0));
+  assert(SDL_JoystickIndex(pad1) == 0);
+  assert(strncmp(SDL_JoystickName(0), "Pad Thai", 9) == 0);
+  assert(strncmp(SDL_JoystickName(1), "Pad Kee Mao", 12) == 0);
+  assert(SDL_JoystickNumAxes(pad1) == 4);
+  assert(SDL_JoystickNumButtons(pad1) == 16);
+
+  // By default, SDL will automatically process events. Test this behavior, and then disable it.
+  assert(SDL_JoystickEventState(SDL_QUERY) == SDL_ENABLE);
+  SDL_JoystickEventState(SDL_DISABLE);
+  assert(SDL_JoystickEventState(SDL_QUERY) == SDL_DISABLE);
+  // Button events.
+  emscripten_run_script("window.simulateGamepadButtonDown(0, 1)");
+  // We didn't tell SDL to automatically update this joystick's state.
+  assertNoJoystickEvent();
+  SDL_JoystickUpdate();
+  assertJoystickEvent(0, SDL_JOYBUTTONDOWN, 1, SDL_PRESSED);
+  assert(SDL_JoystickGetButton(pad1, 1) == 1);
+  // Enable automatic updates.
+  SDL_JoystickEventState(SDL_ENABLE);
+  assert(SDL_JoystickEventState(SDL_QUERY) == SDL_ENABLE);
+  emscripten_run_script("window.simulateGamepadButtonUp(0, 1)");
+  assertJoystickEvent(0, SDL_JOYBUTTONUP, 1, SDL_RELEASED);
+  assert(SDL_JoystickGetButton(pad1, 1) == 0);
+  // No button change: Should not result in a new event.
+  emscripten_run_script("window.simulateGamepadButtonUp(0, 1)");
+  assertNoJoystickEvent();
+  // Joystick 1 is not opened; should not result in a new event.
+  emscripten_run_script("window.simulateGamepadButtonDown(1, 1)");
+  assertNoJoystickEvent();
+
+  // Joystick wiggling
+  emscripten_run_script("window.simulateAxisMotion(0, 0, 1)");
+  assertJoystickEvent(0, SDL_JOYAXISMOTION, 0, 32767);
+  assert(SDL_JoystickGetAxis(pad1, 0) == 32767);
+  emscripten_run_script("window.simulateAxisMotion(0, 0, 0)");
+  assertJoystickEvent(0, SDL_JOYAXISMOTION, 0, 0);
+  assert(SDL_JoystickGetAxis(pad1, 0) == 0);
+  emscripten_run_script("window.simulateAxisMotion(0, 1, -1)");
+  assertJoystickEvent(0, SDL_JOYAXISMOTION, 1, -32768);
+  assert(SDL_JoystickGetAxis(pad1, 1) == -32768);
+  emscripten_run_script("window.simulateAxisMotion(0, 1, -1)");
+  // No joystick change: Should not result in a new event.
+  assertNoJoystickEvent();
+  // Joystick 1 is not opened; should not result in a new event.
+  emscripten_run_script("window.simulateAxisMotion(1, 1, -1)");
+  assertNoJoystickEvent();
+
+  SDL_JoystickClose(pad1);
+  assert(!SDL_JoystickOpened(0));
+
+  // Joystick 0 is closed; we should not process any new gamepad events from it.
+  emscripten_run_script("window.simulateGamepadButtonDown(0, 1)");
+  assertNoJoystickEvent();
+
+  // End test.
+  result = 2;
+  printf("Test passed!\n");
+  REPORT_RESULT();
+  */
+}
+
 int main() {
   if (!glfwInit())
   {
@@ -88,8 +166,9 @@ int main() {
     return -1;
   }
   glfwMakeContextCurrent(g_window);
-
   glfwSetJoystickCallback(joystick_callback);
+
+  emscripten_async_call(main_2, NULL, 3000); // avoid startup delays and intermittent errors
 
 #ifdef __EMSCRIPTEN__
   emscripten_set_main_loop(render, 0, 1);
