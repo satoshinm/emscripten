967914bc107e4b3f521b8897d3f0461e1ee8d04e
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-04-29 15:41:22 -0700

    Test axes, test_glfw_joystick now passes

--- tests/test_glfw_joystick.c
+++ tests/test_glfw_joystick.c
@@ -23,26 +23,26 @@ void render() {
   if (joy_connected == -1) return;
 
   static struct {
-    int axis_count;
-    float axis[16];
+    int axes_count;
+    float axes[16];
     int button_count;
     unsigned char buttons[16];
   } last_gamepad_state = {0};
 
 
-  int axis_count = 0;
-  const float *axis = glfwGetJoystickAxes(joy_connected, &axis_count);
+  int axes_count = 0;
+  const float *axes = glfwGetJoystickAxes(joy_connected, &axes_count);
 
   int button_count = 0;
   const unsigned char *buttons = glfwGetJoystickButtons(joy_connected, &button_count);
 
-  last_gamepad_state.axis_count = axis_count;
-  for (int i = 0; i < axis_count; ++i) {
-    if (last_gamepad_state.axis[i] != axis[i]) {
-      printf("axis %d = %f\n", i, axis[i]);
+  last_gamepad_state.axes_count = axes_count;
+  for (int i = 0; i < axes_count; ++i) {
+    if (last_gamepad_state.axes[i] != axes[i]) {
+      printf("axes %d = %f\n", i, axes[i]);
     }
 
-    last_gamepad_state.axis[i] = axis[i];
+    last_gamepad_state.axes[i] = axes[i];
   }
 
   last_gamepad_state.button_count =  button_count;
@@ -90,7 +90,7 @@ void main_2(void *arg) {
   assert(axes_count == 0);
   assert(buttons_count == 4);
 
-  // Button events.
+  // Buttons
   printf("Testing buttons\n");
   const unsigned char *buttons = glfwGetJoystickButtons(GLFW_JOYSTICK_1, &buttons_count);
   assert(buttons_count == 16);
@@ -105,59 +105,38 @@ void main_2(void *arg) {
   assert(buttons_count == 16);
   assert(buttons[1] == 0);
 
-  /*
-  // By default, SDL will automatically process events. Test this behavior, and then disable it.
-  assert(SDL_JoystickEventState(SDL_QUERY) == SDL_ENABLE);
-  SDL_JoystickEventState(SDL_DISABLE);
-  assert(SDL_JoystickEventState(SDL_QUERY) == SDL_DISABLE);
-  emscripten_run_script("window.simulateGamepadButtonDown(0, 1)");
-  // We didn't tell SDL to automatically update this joystick's state.
-  assertNoJoystickEvent();
-  SDL_JoystickUpdate();
-  assertJoystickEvent(0, SDL_JOYBUTTONDOWN, 1, SDL_PRESSED);
-  assert(SDL_JoystickGetButton(pad1, 1) == 1);
-  // Enable automatic updates.
-  SDL_JoystickEventState(SDL_ENABLE);
-  assert(SDL_JoystickEventState(SDL_QUERY) == SDL_ENABLE);
-  emscripten_run_script("window.simulateGamepadButtonUp(0, 1)");
-  assertJoystickEvent(0, SDL_JOYBUTTONUP, 1, SDL_RELEASED);
-  assert(SDL_JoystickGetButton(pad1, 1) == 0);
-  // No button change: Should not result in a new event.
-  emscripten_run_script("window.simulateGamepadButtonUp(0, 1)");
-  assertNoJoystickEvent();
-  // Joystick 1 is not opened; should not result in a new event.
-  emscripten_run_script("window.simulateGamepadButtonDown(1, 1)");
-  assertNoJoystickEvent();
+
+  emscripten_run_script("window.simulateGamepadButtonDown(1, 0)");
+  buttons = glfwGetJoystickButtons(GLFW_JOYSTICK_2, &buttons_count);
+  assert(buttons_count == 4);
+  assert(buttons[0] == 1);
+
+  emscripten_run_script("window.simulateGamepadButtonUp(1, 0)");
+  buttons = glfwGetJoystickButtons(GLFW_JOYSTICK_2, &buttons_count);
+  assert(buttons_count == 4);
+  assert(buttons[1] == 0);
 
   // Joystick wiggling
+  printf("Testing joystick axes\n");
   emscripten_run_script("window.simulateAxisMotion(0, 0, 1)");
-  assertJoystickEvent(0, SDL_JOYAXISMOTION, 0, 32767);
-  assert(SDL_JoystickGetAxis(pad1, 0) == 32767);
+  const float *axes = glfwGetJoystickAxes(GLFW_JOYSTICK_1, &axes_count);
+  assert(axes_count == 4);
+  assert(axes[0] == 1);
+
   emscripten_run_script("window.simulateAxisMotion(0, 0, 0)");
-  assertJoystickEvent(0, SDL_JOYAXISMOTION, 0, 0);
-  assert(SDL_JoystickGetAxis(pad1, 0) == 0);
-  emscripten_run_script("window.simulateAxisMotion(0, 1, -1)");
-  assertJoystickEvent(0, SDL_JOYAXISMOTION, 1, -32768);
-  assert(SDL_JoystickGetAxis(pad1, 1) == -32768);
-  emscripten_run_script("window.simulateAxisMotion(0, 1, -1)");
-  // No joystick change: Should not result in a new event.
-  assertNoJoystickEvent();
-  // Joystick 1 is not opened; should not result in a new event.
-  emscripten_run_script("window.simulateAxisMotion(1, 1, -1)");
-  assertNoJoystickEvent();
+  axes = glfwGetJoystickAxes(GLFW_JOYSTICK_1, &axes_count);
+  assert(axes_count == 4);
+  assert(axes[0] == 0);
 
-  SDL_JoystickClose(pad1);
-  assert(!SDL_JoystickOpened(0));
-
-  // Joystick 0 is closed; we should not process any new gamepad events from it.
-  emscripten_run_script("window.simulateGamepadButtonDown(0, 1)");
-  assertNoJoystickEvent();
+  emscripten_run_script("window.simulateAxisMotion(0, 1, -1)");
+  axes = glfwGetJoystickAxes(GLFW_JOYSTICK_1, &axes_count);
+  assert(axes_count == 4);
+  assert(axes[1] == -1);
 
   // End test.
   result = 2;
   printf("Test passed!\n");
   REPORT_RESULT();
-  */
 }
 
 int main() {
